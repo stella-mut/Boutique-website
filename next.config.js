79fe2b79d057d9312bdf7b629efec2514780817b
Stella Mutai <stl.mutai@gmail.com>
2026-07-18 22:17:53 +0300

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+    ],
+  },
+};
+
+module.exports = nextConfig;/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+    ],
+  },
+};
+
 module.exports = nextConfig;
-images: {
-  remotePatterns: [
-    { protocol: 'https', hostname: 'images.unsplash.com' },
-    { protocol: 'https', hostname: 'picsum.photos' },
-  ],
-}
