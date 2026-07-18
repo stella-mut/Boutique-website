8a0e64bd21e477e36964f5702188a6bd42859445
Stella Mutai <stl.mutai@gmail.com>
2026-07-18 22:08:02 +0300

    updates

--- next.config.js
+++ next.config.js
@@ -9,3 +9,9 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+images: {
+  remotePatterns: [
+    { protocol: 'https', hostname: 'images.unsplash.com' },
+    { protocol: 'https', hostname: 'picsum.photos' },
+  ],
+}
